39f4644895856757bb156d4ad4e3d7f968feba60
losikvladislav <vlad.losik@yandex.ru>
2023-09-04 14:02:45 +0300

    hw10

--- src.com.teachmeskills.lesson10/part2/Main.java
+++ src.com.teachmeskills.lesson10/part2/Main.java
@@ -4,7 +4,7 @@ import part2.treatment.Treatment;
 
 public class Main {
     public static void main(String[] args) {
-        String str = "Hello My name is Vladislav,i like study";
+        String str = "Hello.My name is Vladislav,i like study";
         System.out.println(Treatment.findShortWord(str));
         System.out.println(Treatment.findLongWord(str));
         System.out.println(Treatment.duplicateLetters(str));
